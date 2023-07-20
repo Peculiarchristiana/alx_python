5a38996aac39820d196c86180c5788f8d1b8597a
Peculiarchristiana <alaooluwakemic@gmail.com>
2023-07-20 20:05:16 +0100

    Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,9 @@
 word = "Holberton"
 print ("First 3 letters: Hol")
 print ("Last 2 letters: on") 
-print ("Middle word: olberto")      
-        
+print ("Middle word: olberto")  
+
+word = "Congratulation"
+print ("First 3 letters: Con")
+print ("Last 2 letters: on") 
+print ("Middle word: ongratulatio")       
