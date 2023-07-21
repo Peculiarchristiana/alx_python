4f2ba188869b31dd4e79fc242f75ce8b4c98059e
Peculiarchristiana <alaooluwakemic@gmail.com>
2023-07-21 12:41:43 +0100

    Recent changes

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
-word = "Holberton"
-print ("First 3 letters: Hol")
-print ("Last 2 letters: on") 
-print ("Middle word: olberto") 
+word = "School"
+print ("First 3 letters: Sch")
+print ("Last 2 letters: ol") 
+print ("Middle word: choo") 
