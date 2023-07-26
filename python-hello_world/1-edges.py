5acd5d6e6f968b430a7d19343fa19a1aa71c3565
Peculiarchristiana <alaooluwakemic@gmail.com>
2023-07-26 17:54:44 +0100

    1-edges

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
-word = "School"
-print ("First 3 letters: Sch")
-print ("Last 2 letters: ol") 
-print ("Middle word: choo") 
+word = "Holberton" 
+print ("First 3 letters: Hol")
+print ("Last 2 letters: on") 
+print ("Middle word: olberto") 
