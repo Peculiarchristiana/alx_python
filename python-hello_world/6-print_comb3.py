f87ff804d6e623e3157bd2c9c78d7c8b7d408f5d
Peculiarchristiana <alaooluwakemic@gmail.com>
2023-08-09 17:08:00 +0100

    hello world task 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -3,7 +3,7 @@
 # differnt combinations of two digits
 for i in range(1,11):
     for j in range(i + 1, 99):
-        print(f"{i:02}, {j:02}", end=', ')
+        print("{:02}, {:02}", format (i), end=', ')
     if i != 9:
         print(f"{i:02}, 10", end=', ')
     else:
