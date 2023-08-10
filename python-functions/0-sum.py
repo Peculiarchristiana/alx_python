2c513c05dfae1b3e3ec7dda2c193f285045e9c28
Peculiarchristiana <alaooluwakemic@gmail.com>
2023-08-10 18:53:10 +0100

    0-sum.

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-#import the '0-sum' module
-add = __import__('0-sum').add
 a = 1
 b = 2
-#functions to add two integers
-def add(a,b):
-    #printing results
-    print(add(a,b))
-    print(add(98, 0))
-    print(add(100, -2))
-    print(add(0, 0))
-add(a,b)
+# Function to add two integers
+def add():
+        add=(a+b)
+        #printing results
+        print(a+b)
+        print(100+-2)
+        print(-100+-2)
+        print(0+0)
+add()
 # and return the sum of those two integer
