d0ff684837d796ca12023393dc4b22f6daeb33bc
Peculiarchristiana <alaooluwakemic@gmail.com>
2023-08-10 19:04:55 +0100

    corrected sum

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-a = 1
-b = 2
+
 # Function to add two integers
-def add():
-        add=(a+b)
-        #printing results
-        print(a+b)
-        print(100+-2)
-        print(-100+-2)
-        print(0+0)
-add()
-# and return the sum of those two integer
+def add(a,b):
+        result= a+b
+        print (result)
+        return (result)
+a=1
+b=2
+        # calling the function with arguments a and b
+add(a,b)  
+add(100,-2)
+add(-100,-2)
+add(0,0)     
