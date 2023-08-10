3d46b6c803c63901e7de21980d385f0d90306445
Peculiarchristiana <alaooluwakemic@gmail.com>
2023-08-10 19:12:07 +0100

    0_sum

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -5,10 +5,14 @@ def add(a,b):
         result= a+b
         print (result)
         return (result)
-a=1
-b=2
+def main():
+    a=1
+    b=2
         # calling the function with arguments a and b
-add(a,b)  
-add(100,-2)
-add(-100,-2)
-add(0,0)     
+    add(a,b)  
+    add(100,-2)
+    add(-100,-2)
+    add(0,0) 
+
+if __name__ == "__main__":
+        main()
